ce68c9d075992c5ce5c1aff0d2b3d650d5dafa1a
Saleh Abdulla <salehabdullaict@gmail.com>
2025-06-20 01:20:48 +0300

    GoLang

---
+++ 1/index.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+var name string = "Saleh";
+
+
+func main(){
+
+	varOne := 100;
+	varTwo := 2;
+
+	fmt.Println(varOne + varTwo);
+
+	var one, two, three, four, five int = 1, 2, 3, 4, 5;
+
+	fmt.Println(one);
+	fmt.Println(two);
+	fmt.Println(three);
+	fmt.Println(four);
+	fmt.Println(five);
+	
+	fmt.Println("Hello World", name);
+
+
+
+  // --------------
+
+	var (
+		num int;
+		number int = 1;
+		greetings string = "hello";
+
+	)
+
+
+	fmt.Println(num);
+	fmt.Println(number);
+	fmt.Println(greetings);
+
+
+}
---
+++ 2/index.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+var number = 110;
+const MY_NAME = "Saleh"
+
+
+const (
+	ISGOLANGPL = true
+	ISHTMLPL = false
+)
+
+
+func main() {
+
+	a := 200;
+	b := 10;
+	c := 10000;
+	
+	num := 1000;
+	fmt.Println(num); 
+	fmt.Println(MY_NAME); 
+	fmt.Println(a); 
+	fmt.Println(b); 
+	fmt.Println(c); 
+	fmt.Println(ISGOLANGPL); 
+	fmt.Println(ISHTMLPL); 
+
+}
+
