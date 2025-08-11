c3a8a1d254adf97d3cf978e631f9fd63a0ff5bd9
Saleh Abdulla <salehabdullaict@gmail.com>
2025-08-11 04:24:13 +0300

    Practice

---
+++ GoLangDSA/PushBeyond/FindPairs/5/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func FindPairs(nums []int, target int) [][]int {
+    var result [][]int
+    used := make(map[int]bool)
+    for i := 0; i < len(nums); i++ {
+        if used[i] {continue}
+        for j := i + 1; j < len(nums); j++ {
+            if used[j] {continue}
+            if nums[i] + nums[j] == target {
+                result = append(result, []int{i, j})
+            }
+        }
+    }
+    return result
+}
+
+func main(){
+    args := os.Args[1:]
+    if len(args) != 2 {fmt.Println("Invalid input."); return }
+    nums := args[0]
+    if len(nums) < 3 || nums[0] != '[' || nums[len(nums)-1] != ']' {fmt.Println("Invalid input."); return }
+    
+    splitNums := strings.Split(nums[1:len(nums)-1], ",")
+    var numToInts []int
+    for _, str := range splitNums {
+        str = strings.TrimSpace(str)
+        strToInt, err := strconv.Atoi(str)
+        if err != nil {fmt.Println("Invalid number: ", strToInt); return }
+        numToInts = append(numToInts, strToInt)
+    }
+    
+    target := args[1]
+    targetToInt, err := strconv.Atoi(target)
+    if err != nil {fmt.Println("Invalid target sum."); return}
+
+    result := FindPairs(numToInts, targetToInt)
+
+    if len(result) == 0 {
+        fmt.Println("No pairs found.")
+    } else {
+        fmt.Printf("Pairs with sum %d: %v", targetToInt, result)
+    }
+}
