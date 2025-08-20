226011a5acc2942fd4186c5aeedd03c864c7afef
Saleh Abdulla <salehabdullaict@gmail.com>
2025-08-21 00:37:28 +0300

    I'm The One

--- GoLangDSA/PushBeyond/fifth_and_skip/3/main.go
+++ GoLangDSA/PushBeyond/fifth_and_skip/3/main.go
@@ -3,24 +3,27 @@ package piscine
 func FifthAndSkip(str string) string {
     if str == "" {return "\n"}
 
-    var result string
-
-    var strToRunes []rune
-    for _, char := range str{
-        if char != ' '{
-            strToRunes = append(strToRunes, char)
-        }
-    }
+    strToRunes := removeSpacesSlice(str)
 
     if len(strToRunes) < 5 {return "Invalid Input\n"}
-    
+
+    var result string
     i := 0
     for i + 5 < len(strToRunes) {
         result += string(strToRunes[i:i+5]) + " "
-        i+=6
+        i += 6
     }
-    if i < len(str) {
+    if i != len(strToRunes) {
         result += string(strToRunes[i:])
     }
     return result + "\n"
+}
+
+func removeSpacesSlice(str string) (strToRunes []rune) {
+    for _, char := range str {
+        if char != ' ' {
+            strToRunes = append(strToRunes, char)
+        }
+    }
+    return
 }
