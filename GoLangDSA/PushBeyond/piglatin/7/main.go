b14ff923ff75c30035375f548617302157810e0d
Saleh Abdulla <salehabdullaict@gmail.com>
2025-08-22 21:40:30 +0300

    I'm The One

---
+++ GoLangDSA/PushBeyond/piglatin/7/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "os"
+    "github.com/01-edu/z01"
+)
+
+// (aeiou)
+func IsVowel(r rune) bool {
+    return r == 'a' || r == 'e' || r == 'i' || r == 'o' || r == 'u' || r == 'A' || r == 'E' || r == 'I' || r == 'O' || r == 'U'
+}
+
+func Print(s string) {
+    for _, char := range s {
+        z01.PrintRune(char)
+    }
+}
+
+func main(){
+    args := os.Args[1:]
+    if len(args) != 1 || args[0] == "" {return}
+
+    vowelIndex := -1
+    word := args[0]
+    for i, char := range word {
+        if IsVowel(char) {
+            vowelIndex = i
+            break
+        }
+    }
+    if vowelIndex == -1 {Print("No vowels\n"); return}
+    if vowelIndex == 0 {
+        Print(word)
+        Print("ay")
+    } else {
+        Print(word[vowelIndex:])
+        Print(word[:vowelIndex])
+        Print("ay")
+    }
+    Print("\n")
+}
+
+
