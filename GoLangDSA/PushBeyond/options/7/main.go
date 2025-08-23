2967962c9d0dc44d44367e5b721e29f3111d9bff
Saleh Abdulla <salehabdullaict@gmail.com>
2025-08-23 19:31:40 +0300

    I'm The One

---
+++ GoLangDSA/PushBeyond/options/7/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("options: abcdefghijklmnopqrstuvwxyz")
+		return
+	}
+	var options uint32
+	for _, arg := range args {
+		if len(arg) < 2 || arg[0] != '-' {
+			fmt.Println("Invalid Option")
+			return
+		}
+
+		for i, char := range arg[1:] {
+			if char < 'a' || char > 'z' {
+				fmt.Println("Invalid Option")
+				return
+			}
+			if char == 'h' && i == 0 {
+				fmt.Println("options: abcdefghijklmnopqrstuvwxyz")
+				return
+			}
+			bit := char - 'a'
+			options |= 1 << bit
+		}
+
+	}
+
+	for i := 31; i >= 0; i-- {
+		if i != 31 && (i+1)%8 == 0 {
+			fmt.Print(" ")
+		}
+		bit := (options >> i) & 1
+		fmt.Print(bit)
+	}
+
+	fmt.Println()
+}
