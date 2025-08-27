8a343c6598d01a0b45deeab3060130decc07fcb7
Saleh Abdulla <salehabdullaict@gmail.com>
2025-08-27 03:24:23 +0300

    I'm The One

---
+++ GoLangDSA/PushBeyond/RegExpTasks/4/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	data, err := os.ReadFile("sample.txt")
+	if err != nil {fmt.Println("Error reading file:", err); return}
+	text := string(data)
+	fmt.Println(text)
+
+	re, err := regexp.Compile(`(?im)world\r?$`) // \r because of windows
+	if err != nil {
+		fmt.Println("Error compiling regex:", err)
+		return
+	}
+
+	matches := re.FindAllString(text, -1)
+
+	for i, match := range matches {
+		fmt.Printf("%d: %s\n", i+1, match)
+	}
+}
