57018db8e912d8877312a75b1e20bca063f7c06b
Saleh Abdulla <salehabdullaict@gmail.com>
2025-08-29 18:11:13 +0300

    I'm The One

---
+++ GoLangDSA/PushBeyond/RegExpTasks/RemoveSpacesBeforeCommas.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"regexp"
+	"fmt"
+)
+
+
+func main(){
+	text := "Hello \t     \r\n   , World!"
+	cmd := `\s+([.,!?;:])`
+	repl := "$1"
+	text = MustComplie(text, cmd, repl)
+	fmt.Println(text)
+}
+
+func MustComplie(s, command, replacement string) string {
+	re := regexp.MustCompile(command)
+	return re.ReplaceAllString(s, replacement)
+}
