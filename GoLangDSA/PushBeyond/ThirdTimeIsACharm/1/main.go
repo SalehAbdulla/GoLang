e1313632f67959b6be7d8f7ca232273158fe2fda
Saleh Abdulla <salehabdullaict@gmail.com>
2025-09-12 16:52:43 +0300

    I'm Worthy

---
+++ GoLangDSA/PushBeyond/ThirdTimeIsACharm/1/main.go
@@ -0,0 +1,10 @@
+package piscine
+
+
+func ThirdTimeIsACharm(str string) string {
+    var result string
+    for i := 2; i < len(str); i+=3{
+        result += string(str[i])
+    }
+    return result + "\n"
+}
