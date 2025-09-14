223e4716393c4cb8b5a33408c6dbb719a82795ea
Saleh Abdulla <salehabdullaict@gmail.com>
2025-09-14 15:29:24 +0300

    I'm The One

---
+++ GoLangDSA/PushBeyond/itoa_base/10/main.go
@@ -0,0 +1,30 @@
+package piscine
+
+const digits = "0123456789ABCDEF"
+
+func ItoaBase(value, base int) string {
+    if value == 0 { return "0" }
+    var uValue uint64
+    isNegative := value < 0
+    if isNegative {
+        uValue = uint64(-int64(value))
+    } else {
+        uValue = uint64(value)
+    }
+
+    uBase := uint64(base)
+
+    var result string
+
+    for uValue > 0 {
+        reminder := uValue % uBase
+        result = string(digits[reminder]) + result
+        uValue/=uBase
+    }
+
+
+    if isNegative {
+        return "-" + result
+    }
+    return result
+}
