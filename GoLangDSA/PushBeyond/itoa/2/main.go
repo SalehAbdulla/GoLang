715519b6b079ad3684d3ac3bae2a474eab391ca5
Saleh Abdulla <salehabdullaict@gmail.com>
2025-09-21 21:29:13 +0300

    I'm The One

---
+++ GoLangDSA/PushBeyond/itoa/2/main.go
@@ -0,0 +1,23 @@
+package piscine
+
+
+func Itoa(n int) string {
+    if n == 0 {return "0"}
+    isNegative := false
+    if n < 0 {
+        isNegative = true
+        n = -n
+    }
+    var result string
+
+    for n > 0 {
+        digit := n %10
+        result = string(rune(digit + '0')) + result
+        n /= 10
+    }
+
+    if isNegative {
+        return "-" + result
+    }
+    return result
+}
