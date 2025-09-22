22daf9abbd97ba93a99fba93bf0704ef8415325f
Saleh Abdulla <salehabdullaict@gmail.com>
2025-09-22 18:03:37 +0300

    I'm The One

---
+++ GoLangDSA/PushBeyond/FindPrevPrime/3/main.go
@@ -0,0 +1,24 @@
+package piscine
+
+func FindPrevPrime(nb int) int {
+	for i := nb; i >= 2; i-- {
+		if IsPrime(i) {
+			return i
+		}
+	}
+	return 0
+}
+
+func IsPrime(num int) bool {
+	if num < 2 {
+		return false
+	}
+	i := 2
+	for i*i <= num {
+		if num%i == 0 {
+			return false
+		}
+		i++
+	}
+	return true
+}
