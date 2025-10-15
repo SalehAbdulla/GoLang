a1bfd410252392eeb485919c738e872bef5bd57d
Saleh Abdulla <salehabdullaict@gmail.com>
2025-10-15 23:31:57 +0300

    I'm the One

---
+++ GoLangDSA/PushBeyond/FindPairs/14/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "os"
+    "fmt"
+)
+
+
+
+func main(){
+    args := os.Args[1:]
+    if len(args) != 2 {fmt.Println("Invalid input."); return}
+    
+    nums := args[0]
+    if len(nums) < 3 || nums[0] != "[" || nums[len(nums)-1] != ']' {fmt.Println("Invalid input."); return}
+    
+    nums = nums[1:len(nums)-1] // removes [] brackets
+    nums = strings.ReplaceAll(nums, " ", "")
+    
+    numsSlice = strings.Split(nums, ",")
+    target := args[0]
+}
