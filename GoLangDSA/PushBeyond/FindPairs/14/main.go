13b4116314d87a46fd9cccb87c745fe73cd2ed2d
Saleh Abdulla <salehabdullaict@gmail.com>
2025-10-16 23:59:26 +0300

    I'm The One

--- GoLangDSA/PushBeyond/FindPairs/14/main.go
+++ GoLangDSA/PushBeyond/FindPairs/14/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-    "os"
-    "fmt"
+	"fmt"
+	"os"
+	"strings"
 )
 
+func main() {
+	args := os.Args[1:]
+	if len(args) != 2 {
+		fmt.Println("Invalid input.")
+		return
+	}
 
+	nums := args[0]
+	if len(nums) < 3 || nums[0] != '[' || nums[len(nums)-1] != ']' {
+		fmt.Println("Invalid input.")
+		return
+	}
 
-func main(){
-    args := os.Args[1:]
-    if len(args) != 2 {fmt.Println("Invalid input."); return}
-    
-    nums := args[0]
-    if len(nums) < 3 || nums[0] != "[" || nums[len(nums)-1] != ']' {fmt.Println("Invalid input."); return}
-    
-    nums = nums[1:len(nums)-1] // removes [] brackets
-    nums = strings.ReplaceAll(nums, " ", "")
-    
-    numsSlice = strings.Split(nums, ",")
-    target := args[0]
+	nums = nums[1 : len(nums)-1] // removes [] brackets
+	nums = strings.ReplaceAll(nums, " ", "")
+
+	numsSlice = strings.Split(nums, ",")
+	target := args[0]
 }
