66a620f52da3e03cac22f99e93a60e9e09a72a64
Saleh Abdulla <salehabdullaict@gmail.com>
2025-10-20 22:02:30 +0300

    I'm The One

---
+++ GoLangDSA/PushBeyond/rpncalc/13/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "strings"
+    "strconv"
+)
+
+func rpnCalc(strs []string) (int, bool) {
+    var stack []int
+    for _, str := range strs {
+        if str == "" {continue}
+        strToInt, err := strconv.Atoi(str)
+        if err == nil {stack = append(stack, strToInt); continue}
+        if len(stack) < 2 {return -1, false}
+        switch str {
+            case "+":
+                stack[len(stack)-2] += stack[len(stack)-1]
+                stack = stack[:len(stack)-1] //pop
+            case "-":
+                stack[len(stack)-2] -= stack[len(stack)-1]
+                stack = stack[:len(stack)-1] //pop
+            case "*":
+                stack[len(stack)-2] *= stack[len(stack)-1]
+                stack = stack[:len(stack)-1] //pop
+            case "/":
+                stack[len(stack)-2] /= stack[len(stack)-1]
+                stack = stack[:len(stack)-1] //pop
+            case "%":
+                stack[len(stack)-2] %= stack[len(stack)-1]
+                stack = stack[:len(stack)-1] //pop
+            default:
+                return -1, false                
+        } 
+    }
+    if len(stack) == 1 {return stack[0], true}
+    return -1, false
+}
+
+func main(){
+    args := os.Args[1:]
+    if len(args) != 1 {fmt.Println("Error"); return}
+
+    var inputSlice []string
+    inputSlice = strings.Split(args[0], " ")
+    solution, ok := rpnCalc(inputSlice)
+
+    if !ok {fmt.Println("Error"); return}
+    fmt.Println(solution)
+}
